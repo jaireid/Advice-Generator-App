52706763551932ed67a78827c7c1d840ff607a96
beareid <beareid.info@gmail.com>
2022-06-04 03:53:27 -0500

    Update how advice is loaded in app
    Convert fetch method into a function and call function in event listener method to randomly generate advice when the button is clicked

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 // variables
-const button = document.querySelector('button');
-const number = document.querySelector('.number');
-const quote = document.querySelector('.quote');
+const button = document.querySelector("button");
+const number = document.querySelector(".number");
+const quote = document.querySelector(".quote");
 
-// fetch API
-fetch('https://api.adviceslip.com/advice')
-  .then(data => {
-    // Converts data into json
-    return data.json();
-  })
-  .then(completedata => {
-
-    const id = completedata["slip"].id;
-    const advice = completedata["slip"].advice;
-    // Test to see if data was pulled from object
-    console.log(id,advice);
-    // Assigning the object data to DOM elements
-    number.textContent = id;
-    quote.textContent = advice;
-    // Event Listener to refresh page when button is clicked
-    button.addEventListener('click', () => {
-      location.reload();
+// Function for fetch API
+const getAdvice = () => {
+  fetch("https://api.adviceslip.com/advice")
+    .then((data) => {
+      // Converts data from api into json
+      return data.json();
+    })
+    .then((completedata) => {
+      const id = completedata["slip"].id;
+      const advice = completedata["slip"].advice;
+      // Test to see if data was pulled from object
+      console.log(id, advice);
+      // Assigning the object data to DOM elements
+      number.textContent = id;
+      quote.textContent = advice;
     });
-  });
+};
 
+// Event Listener that calls the advice function when the button is clicked
+button.addEventListener("click", getAdvice);
+
+// Initial call to the getAdvice function that loads advice to the page
+getAdvice();
